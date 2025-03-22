c65c333b141e43833a1fb3f8e6faae56543b0d62
Pierre <grandett1@gmail.com>
2025-03-22 11:24:14 -0500

    Correccion fomat, intro fundamentos

--- format.py
+++ format.py
@@ -1,6 +1,15 @@
+import sys
+
 """Module providing a function printing python version."""
 
 
 chanchito = "feliz"
 a = 12
 b = 12
+def print_python_version():
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
+
+print_python_version()
